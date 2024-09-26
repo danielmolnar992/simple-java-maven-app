bbbdc5e68345589022e71d02ffb1dbfa33663b5b
danielmolnar992 <90219172+danielmolnar992@users.noreply.github.com>
2024-09-26 07:59:55 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,8 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-ls -a
-cd target
-ls -a
-cd ..
 java -jar target/${NAME}-${VERSION}.jar
