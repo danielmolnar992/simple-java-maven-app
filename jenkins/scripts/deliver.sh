6ce250cfa655aba2fc65a0634555db88bff429bd
danielmolnar992 <90219172+danielmolnar992@users.noreply.github.com>
2024-09-26 23:05:10 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,6 +25,4 @@ echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 ls -l target/
 java -jar 'target/my-app-1.0-SNAPSHOT.jar'
-java -jar target/my-app-1.0-SNAPSHOT.jar
-java -jar target/"${NAME}"-"${VERSION}".jar
 java -jar target/${NAME}-${VERSION}.jar
